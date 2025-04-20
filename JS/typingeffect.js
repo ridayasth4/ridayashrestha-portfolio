5de81651fa93d1b4058c8ded61144699381fa75a
Hridaya Shrestha <129663764+cherrrydo@users.noreply.github.com>
2025-04-20 14:44:31 +0545

    Update typingeffect.js

--- JS/typingeffect.js
+++ JS/typingeffect.js
@@ -1,6 +1,6 @@
 // word transition effect
 document.addEventListener("DOMContentLoaded", ()=>{
-    const roles = ['computer Science Student', 'IT Student'];
+    const roles = ['Computer Science Student', 'IT Student'];
     const roleElement = document.getElementById('role');
     let roleIndex = 0;
     let letterIndex = 0;
