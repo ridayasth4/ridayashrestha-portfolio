e74b7deb8bef87a56c1c9d8e1cebb62853c80d87
Hridaya Shrestha <ridaya350@gmail.com>
2025-04-20 14:40:24 +0545

    Refined typing effect and adjusted HTML layout

--- JS/typingeffect.js
+++ JS/typingeffect.js
@@ -1,6 +1,6 @@
 // word transition effect
 document.addEventListener("DOMContentLoaded", ()=>{
-    const roles = ['Computer Science Student', 'Information Technology Student'];
+    const roles = ['computer Science Student', 'IT Student'];
     const roleElement = document.getElementById('role');
     let roleIndex = 0;
     let letterIndex = 0;
