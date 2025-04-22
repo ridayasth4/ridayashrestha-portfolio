57db0012ded0fc80a71d24f9720ca2b9f81fc44d
Hridaya Shrestha <ridaya350@gmail.com>
2025-04-22 07:04:01 +0545

    morning

--- JS/typingeffect.js
+++ JS/typingeffect.js
@@ -33,3 +33,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
     typeRole();
 });
 
+//project filter
+document.addEventListener("DOMContentLoaded", function(){
+    const filterButtons = document.querySelectorAll('.project-list li');
+    const projects = document.querySelectorAll('.project-box');
+
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", ()=>{
+            filterButtons.forEach((btn)=> btn.classList.remove("active"));
+            button.classList.add("active");
+
+            const filter = button.getAttribute("data-filter");
+
+            projects.forEach((project)=>{
+                if (filter === "all" || project.getAttribute("data-category") === filter){
+                    project.style.display = "block";
+                }
+                else{
+                    project.style.display = "none";
+                }
+            });
+        });
+    });
+});
+
